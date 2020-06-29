70e25f369f81bd17413bab206809c21e30c0bd62
Domy13 <67584316+Domy13@users.noreply.github.com>
2020-06-29 15:27:22 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const client = new Discord.Client()
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
+client.on("guildMemberAdd", member => {
+  const WelcomeChanel = member.guild.channels.cache.find(channel => channel.name === '📝naplata-štete')
+  WelcomeChanel.send ('⚠️🔞  ** $(memmber) SE PRIDRUŽIO SERVERU!**  (*šteta 5000€*)')
+
+})
 client.on("message", msg => {
   if (msg.content === "Janko") {
     msg.reply("ja sam vaš spiritualni vođa, što trebate(TEST)?")
