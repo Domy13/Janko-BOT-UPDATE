ea6d18b5ebb3e4ea74b562e724e816875f473745
Domy13 <67584316+Domy13@users.noreply.github.com>
2020-08-21 17:33:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 const client = new Discord.Client()
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
-  client.user.setActivity('YouTube', { type: 'WATCHING' })
+  client.user.setActivity('Fortnite', { type: 'PLAYING' })
 })
 client.on("guildMemberAdd", member => {
   const WelcomeChanel = member.guild.channels.cache.find(channel => channel.name === '📝naplata-štete')
